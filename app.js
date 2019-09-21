a8d307dd39bc8fa2c3c62857508ad0a0b7001583
Student <student@itpark.ru>
2019-09-21 10:56:34 +0300

    Вариант со 100% возможностью оплаты бонусами

---
+++ app.js
@@ -0,0 +1,11 @@
+const purchaseAmount = 1000;
+const bonuses = 900;
+
+let total;
+if (purchaseAmount <= bonuses) {
+    total = 0;
+} else {
+    total = purchaseAmount - bonuses;
+}
+
+console.log(total);
